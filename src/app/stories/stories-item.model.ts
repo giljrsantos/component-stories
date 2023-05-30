8d1a7bd0ec07d71e1ffea2c0d4b6fd3c301e6db1
Gilberto Jr <giljrsantos@gmail.com>
2023-05-30 20:38:08 -0300

    Desenvolvimento stories-item

--- src/app/stories/stories-item.model.ts
+++ src/app/stories/stories-item.model.ts
@@ -1,1 +1,12 @@
-export class StoriesItem {}
+export interface StoriesItem {
+  descricao: string;
+  descricaoCorHex: string;
+  status: StoriesItemStatus;
+  urlImagem: string;
+}
+
+export enum StoriesItemStatus {
+  'ativo' = 'ativo',
+  'aguardando' = 'aguardando',
+  'finalizado' = 'finalizado',
+}
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,12 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+} from '@angular/core';
+import {
+  StoriesItem,
+  StoriesItemStatus,
+} from './stories-item.model';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
   styleUrls: ['./stories.component.css'],
 })
-export class StoriesComponent implements OnInit {
-  constructor() {}
+export class StoriesComponent
+  implements OnChanges, OnDestroy
+{
+  private controleIntervalo!: Subscription;
 
-  ngOnInit(): void {}
+  @Input()
+  intervaloEntreSlidesEmSegundos: number = 3;
+
+  @Input()
+  itens: StoriesItem[] = [];
+
+  // Retorna o index do array "itens" que está com o status ativo
+  get slideAtivoIndex(): number {
+    return this.itens.findIndex(
+      (a) => a.status === StoriesItemStatus.ativo
+    );
+  }
+
+  // Retorna o item do array "itens" que está com o status ativo
+  get slideAtivo(): StoriesItem {
+    return this.itens[this.slideAtivoIndex];
+  }
+
+  // Quando o componente receber um valor
+  // em algums @Input, este método será
+  // executado
+
+  ngOnChanges() {
+    this.reset();
+  }
+
+  ngOnDestroy() {
+    this.pararTimer();
+  }
+
+  reset() {
+    this.pararTimer();
+
+    this.itens.forEach(
+      (a) => (a.status = StoriesItemStatus.aguardando)
+    );
+    this.itens[0].status = StoriesItemStatus.ativo;
+
+    this.iniciarTimer();
+  }
+
+  iniciarTimer(): void {
+    this.controleIntervalo = timer(
+      this.intervaloEntreSlidesEmSegundos * 1e3
+    ).subscribe(() => {
+      this.avancarSlide();
+    });
+  }
+
+  avancarSlide(): void {
+    this.irParaSlide(this.slideAtivoIndex + 1);
+  }
+
+  pararTimer(): void {
+    this.controleIntervalo?.unsubscribe();
+  }
+
+  irParaSlide(index: number) {
+    this.pararTimer();
+
+    if (index >= this.itens.length) {
+      index = 0;
+    } else if (index < 0) {
+      index = this.itens.length - 1;
+    }
+
+    for (let i = 0; i < index; i++) {
+      this.itens[i].status = StoriesItemStatus.finalizado;
+    }
+
+    this.itens[index].status = StoriesItemStatus.ativo;
+
+    for (let i = index + 1; i < this.itens.length; i++) {
+      this.itens[i].status = StoriesItemStatus.aguardando;
+    }
+
+    this.iniciarTimer();
+  }
 }
