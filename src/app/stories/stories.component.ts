02c83f312cd318b87b2fd1434874d4b3a968ea27
Gilberto Jr <giljrsantos@gmail.com>
2023-05-30 19:53:40 -0300

    Commit inicial

---
+++ src/app/stories/stories.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.css'],
+})
+export class StoriesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/stories/stories.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoriesComponent } from './stories.component';
+
+@NgModule({
+  declarations: [StoriesComponent],
+  imports: [CommonModule],
+})
+export class StoriesModule {}
