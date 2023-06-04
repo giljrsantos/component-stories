914cf8c8e4b2fb1e72453a9fa771db9446cbeff3
Gilberto Jr <giljrsantos@gmail.com>
2023-06-04 11:09:33 -0300

    mecanismo para gerenciar o conteúdo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { StoriesItem, StoriesItemStatus } from './stories/stories-item.model';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  itens: StoriesItem[] = [
+    {
+      descricao: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, modi!',
+      descricaoCorHex:'#fff',
+      status: StoriesItemStatus.aguardando,
+      urlImagem: 'https://wallpapers.com/images/hd/splashing-water-gradient-background-mobile-v5hlk4ta55rn5w3x.jpg'
+    },
+    {
+      descricao: 'Incidunt, modi!',
+      descricaoCorHex:'#fff',
+      status: StoriesItemStatus.aguardando,
+      urlImagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQQjw1nyTi1qvWVyCufBIEuFx8B6Zn9TOLVw&usqp=CAU'
+    },
+    {
+      descricao: 'Lorem ipsum dolor sit amet.',
+      descricaoCorHex:'#fff',
+      status: StoriesItemStatus.aguardando,
+      urlImagem: 'https://marketplace.canva.com/EAFJd1mhO-c/1/0/900w/canva-colorful-watercolor-painting-phone-wallpaper-qq02VzvX2Nc.jpg'
+    }
+  ]
   title = 'projeto-stories';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoriesModule } from './stories/stories.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoriesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
